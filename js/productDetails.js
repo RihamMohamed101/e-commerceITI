0530d929bfaf13c8cc01a78fa85381a318b41a37
Ahmed Mohamed Mohamed mahran <ahmed.mahran824@gmail.com>
2026-01-26 01:17:33 +0200

    Fetch products from JSON and log to console

--- js/productDetails.js
+++ js/productDetails.js
@@ -1,1 +1,7 @@
-
+let products = null;
+        fetch('products.json')
+            .then(response => response.json())
+            .then(data => {
+                products = data;
+                console.log(products);
+            })
